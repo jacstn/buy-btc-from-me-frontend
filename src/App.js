e5367cc26274afae62e2e4aa8d840ea058c19a57
Jacek Stan <jacek.stanusz@gmail.com>
2022-07-12 17:56:09 +0200

    added recent transactions area

--- src/App.js
+++ src/App.js
@@ -1,49 +1,79 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import Axios from 'axios'
-import { Button, Row, Col, Container, Form, Card } from 'react-bootstrap';
+import { Button, Row, Col, Container, Form, Card, Table } from 'react-bootstrap';
 
 const baseUrl = 'http://localhost:3333'
+function getStatus(el) {
+  switch (el.status) {
+    case 'INIT':
+      return 'Init';
+    case 'DONE':
+      return 'Done';
+    case 'BTC_SENT':
+      return `Bitcoin Sent (#${el.blockchainConfirmations} confirmations)`;
+    default:
+      return el.status
+  }
+}
 
 function App() {
-  const [omisePaymentToken, setOmisePaymentToken] = useState()
   const [paymentStatus, setPaymentStatus] = useState();
-  const [btcAddress, setBtcAddress] = useState(10000);
   const [btcPrice, setBtcPrice] = useState(null);
   const [usdValue, setUsdValue] = useState(0);
   const [addressInvalid, setAddressInvalid] = useState();
   const [btcBalance, setBtcBalance] = useState();
+  const [refreshKey, setRefreshKey] = useState(0);
+  const [orders, setOrders] = useState()
 
+  const [working, setWorking] = useState(true)
   useEffect(() => {
-    Axios.get(baseUrl + "/btc-price").then((resp) => {
-      setBtcPrice(resp.data.btcPrice)
+    Axios.get(baseUrl + "/btc-price").then((btcPriceResp) => {
+      setBtcPrice(btcPriceResp.data.btcPrice)
+      Axios.get(baseUrl + "/balance").then((balanceResp) => {
+        setBtcBalance(balanceResp.data.balance)
+        Axios.get(baseUrl + "/order").then((resp) => {
+          setOrders(resp.data)
+          setWorking(false)
+          console.log(resp.data)
+        })
+      })
     })
+  }, [refreshKey])
 
-    Axios.get(baseUrl + "/balance").then((resp) => {
-      setBtcBalance(resp.data.balance)
-    })
-  }, [])
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target));
+    const omiseAmount = (+usdValue) * 100;
+    console.log(data)
+    const createOrderResp = (await Axios.post(baseUrl + "/order", {
+      btcAmount: +(data.btcAmount),
+      usdAmount: usdValue,
+      address: data.address,
+    }));
 
-  async function pay() {
-    const createOrderResp = await Axios.post(baseUrl + "/order", {
-      amount: 123,
-      address: "23423423423423"
-    });
-    console.log('create order response', createOrderResp)
+    console.log('create order response', createOrderResp);
+
     const { OmiseCard } = window
+
+
     OmiseCard.configure({
       publicKey: createOrderResp.data.omisePublicKey,
-      amount: btcAddress
+      amount: omiseAmount,
     })
 
     OmiseCard.open({
-      amount: btcAddress,
+      amount: omiseAmount,
       currency: "USD",
       defaultPaymentMethod: "credit_card",
-      onCreateTokenSuccess: (nonce) => {
-        console.log("omise payment gateway response:", nonce)
+      onCreateTokenSuccess: async (nonce) => {
         if (nonce.startsWith("tokn_")) {
-          setOmisePaymentToken(nonce);
+          await Axios.post(baseUrl + "/charge", {
+            omiseToken: nonce,
+            orderId: createOrderResp.data.id,
+          });
+          setPaymentStatus('verification')
+          setRefreshKey(key => key + 1)
         }
       }
     })
@@ -54,12 +84,8 @@ function App() {
     Axios.get(`${baseUrl}/validate-address/${e.target.value}`).then((resp) => {
       console.log(resp.status)
       setAddressInvalid(false)
+      setRefreshKey(key => key + 1)
     }).catch((e) => setAddressInvalid(true))
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = Object.fromEntries(new FormData(e.target));
   }
 
   const amountChange = (e) => {
@@ -69,7 +95,7 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <Row>
+        <Row className='top-space'>
           <Col>
             <Card className='btc-header'>
               <h3>Price for 1 BTC</h3> <h1>{(btcPrice === null) ? "" : (`${btcPrice} USD`)}</h1>
@@ -90,8 +116,8 @@ function App() {
                 Address invalid
               </div>
               <Form.Label>Enter Amount</Form.Label>
-              <Form.Control onChange={amountChange} name="amount" type="text"></Form.Control>
-              <Button disabled={isNaN(usdValue) || usdValue === 0} className="btn btn-primary pay-button" type="submit">Pay {usdValue > 0 ? `${usdValue} USD` : ""}</Button>
+              <Form.Control onChange={amountChange} name="btcAmount" type="text"></Form.Control>
+              <Button disabled={isNaN(usdValue) || usdValue === 0 || working == true} className="btn btn-primary top-space" type="submit">Pay {usdValue > 0 ? `${usdValue} USD` : ""}</Button>
             </Form>
           </Col>
         </Row>
@@ -100,6 +126,33 @@ function App() {
             {paymentStatus === 'verification' ? (<Col>Payment Verification</Col>) : ""}
             {paymentStatus === 'success' ? (<Col>Payment Successful</Col>) : ""}
             {paymentStatus === 'failed' ? (<Col>Payment Failed</Col>) : ""}
+          </Col>
+        </Row>
+        <Row>
+          <Col><h4 className='top-space'>Recent Transactions</h4></Col>
+        </Row>
+        <Row>
+          <Col>
+            <Table className='top-space' striped bordered hover>
+              <thead>
+                <tr>
+                  <th>BTC</th>
+                  <th>Address</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((val, key) => {
+                  return (
+                    <tr key={key}>
+                      <td>{val.btcAmount}</td>
+                      <td>{val.address}</td>
+                      <td>{getStatus(val)}</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </Table>
           </Col>
         </Row>
       </Container>
